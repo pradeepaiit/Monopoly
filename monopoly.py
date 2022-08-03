a04a18a3026f56395eb3f96d49b9ab465e81a70b
pradeepa panneerselvam <pradeepaiit.95@gmail.com>
2022-08-03 12:01:39 +1000

    update

--- monopoly.py
+++ monopoly.py
@@ -1,21 +1,24 @@
 import json
 import array as arr
-from operator import index
-from unicodedata import name
+# from multiprocessing.sharedctypes import Value
+# from operator import index
+# from unicodedata import name
 
-from numpy import double
+# from numpy import double, tile
 
-Peter = 16
-Billy = 16
-Charlotte = 16
-Sweedal = 16
+#DEFINING VARIABLES
 
-Players = [Peter, Billy, Charlotte, Sweedal]
+round_number = 0
+players_list = ["Peter", "Billy", "Charlotte", "Sweedal"]
+score_list=[16,16,16,16]
 
-total = 0
-game = True
-# double = True
-totalDoubles = 0
+# total = 0
+# game = True
+# totalDoubles = 0
+
+
+
+#TAKING VALUES FROM JSON
 
 x = open('board.json')
 y = open('rolls_1.json')
@@ -28,38 +31,35 @@ c = json.load(z)
 arr1 =arr.array('i',b)
 arr2 =arr.array('f',c)
 
-# initial = a[0]
-
-# def getNext(x):
-#     next_el = None
-
-# for name, elem in enumerate(arr1):
-
-#     if name + 4 < len(arr1) and name - 4 >= 0:
-#         curr_el = elem - 4
-#         next_el = arr1[name]
-        
-#         print("Billy's turn:", next_el)
 
 
-for i in arr1[slice(None,None,4)]:
- print("Peter's dice 1:", i)
-
-for f in arr2[slice(None,None,4)]:
- print("Peter's dice 2:", f)
+# for i in arr1[slice(None,None,4)]:
+#  print("Peter's dice 1:", i)
 
 
-total = i + f
 
-Peter_value = (total)
-Billy_value = (arr1[1])
-Charlotte_value = (arr1[2])
-Sweedal_value = (arr1[3])
+# for f in arr2[slice(None,None,4)]:
+#  print("Peter's dice 2:", f)
 
-print("Peter's roll", total)
+#  total = i + f
+
+#MAKING LOOP BETWEEN 4 PLAYERS
+while True:
+    round_number += 1
+print("Round:", round_number)
+if round_number%4:
+    total = i + f
+    print(players_list[0], "Turn" , total)
+   
+else:
+    print(players_list[1], "Turn")
 
 
-# print("Peter's turn:", total, initial['name'])
-# print("Billy's turn:", Billy)
-# print("Charlotte's turn:", Charlotte)
-# print("Sweedal's turn:", Sweedal)
+
+# Peter_value = (total)
+# Billy_value = (arr1[1])
+# Charlotte_value = (arr1[2])
+# Sweedal_value = (arr1[3])
+
+# print("Peter's roll", total)
+
